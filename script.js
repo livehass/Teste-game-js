d34806023fd0a47c77269e5a8e74c0556825c018
livehass <silva.felipe12@hotmail.com>
2023-08-22 18:17:17 -0300

    commit de update

---
+++ script.js
@@ -0,0 +1,28 @@
+const areaDoJogo = document.querySelector(".area-do-jogo");
+const ElementomaiorPontuacao = document.querySelector(".maior-pontuacao");
+const Elementopontuacao = document.querySelector(".pontuacao");
+const controles = document.querySelectorAll(".constrols i");
+
+console.log(controles);
+
+var fimDeJogo = false;
+var xDaComida, yDaComida;
+var xDacobra = 5, yDaCobra = 5;
+var xDaVelocidade = 0, yDaVelocidade = 0;
+var corpoDaCobra = [];
+var tempoDeEspera;
+var pontuacao = 0;
+
+var maiorPontuacao = localStorage.getItem("maior-pontuacao") || 0;
+ElementomaiorPontuacao.innerText = `Maior Pontuação: ${maiorPontuacao}`;
+
+const atualizaPosicaoDaComida = () => {
+    xDaComida = Math.floor(Math.random() * 30) + 1;
+    yDaComida = Math.floor(Math.random() * 30) + 1;
+}
+
+const AtualizaGamerOver = () => {
+    clearInterval(tempoDeEspera);
+    alert("Fim de jogo, Pressione OK para jogar novamente");
+    location.reload();
+}
